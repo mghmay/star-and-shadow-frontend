449741b6a6a5cfc543079c9fbd18ac48fcf65395
Mat <mghmay@gmail.com>
2022-08-24 22:12:09 +0100

    Update search-bar.component.ts

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { environment as ENV } from 'src/environments/environment';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,12 +9,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
+  categories: object = ENV.CATEGORY_MODEL.data;
+  currentCategory: string = '';
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  changeCategory(e: any): void {
+    this.currentCategory = e.target.value;
+    this.router.navigate([this.currentCategory]);
+  }
+
   onSubmit(form: NgForm): void {
     if (form.value.search !== '') {
-      this.router.navigate(['search', form.value.search]);
+      if (this.currentCategory !== '') {
+        this.router.navigate([this.currentCategory], {
+          queryParams: { title: form.value.search },
+        });
+      } else {
+        this.router.navigate([''], {
+          queryParams: { title: form.value.search },
+        });
+      }
     }
+    form.reset();
   }
 }

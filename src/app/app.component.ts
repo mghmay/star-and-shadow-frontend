cad896199fe2cccdb6c44ab2b325e4681d4e2c6c
Mathew May <mghmay@gmail.com>
2022-08-25 13:33:21 +0100

    Working loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { delay, Subscription } from 'rxjs';
+import { LoadingService } from './services/loading.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Star and Shadow';
+export class AppComponent implements OnInit {
+  loadingSub: Subscription = new Subscription();
+  loading: boolean = false;
+
+  constructor(private loader: LoadingService) {}
+
+  ngOnInit() {
+    this.listenToLoading();
+  }
+
+  listenToLoading(): void {
+    this.loader.loading
+      .pipe(delay(0)) // This prevents a ExpressionChangedAfterItHasBeenCheckedError for subsequent requests
+      .subscribe((loading: any) => {
+        this.loading = loading;
+      });
+  }
 }
